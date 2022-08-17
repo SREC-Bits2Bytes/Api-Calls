9541b278b421ecdd8011d7a4fb3f0c8c5a0f8400
varniha <varniha.2001247@gmail.com>
2022-08-17 21:19:17 +0530

    Calls

--- src/components/AxGetSubjectAtt.js
+++ src/components/AxGetSubjectAtt.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 
 <script src="https://js.stripe.com/v3/"></script>
@@ -11,12 +11,12 @@ const token = JSON.parse(sessionStorage.getItem('data'));
 
 export default function AxGetSubjectAtt() {
   //const [jsonData2,setjsonDepartment] =useState([]);
-  const [jsonData1,setjsondata] =useState([]);
+  const [jsonData,setjsondata] =useState([]);
   const fetchData = async () => {
     try {
       const response = await axios.get(api, {
         headers: {
-          "Access-Token": 'eyJraWQiOiJ0eU9LVHFIV1J2UDlNOGRVNDVQaHhRZkZDTndqRDQ4R3ltbVg4RDhmVmpFPSIsImFsZyI6IlJTMjU2In0.eyJzdWIiOiI5OTg5NzAzYi1mYmIzLTQxYjEtOTk1NS1lNjU2M2Y0YTVjMDMiLCJpc3MiOiJodHRwczpcL1wvY29nbml0by1pZHAuYXAtc291dGgtMS5hbWF6b25hd3MuY29tXC9hcC1zb3V0aC0xX1ZiTUN4V1VxbyIsInZlcnNpb24iOjIsImNsaWVudF9pZCI6IjVkaDBpM3NjODh0N2Jwa2Q4b3FsMzMxYWVqIiwiZXZlbnRfaWQiOiIwZjJiMjU3Mi1kMGUzLTRjMWMtOTgwNi1hMDM0YmE2N2Q3NGEiLCJ0b2tlbl91c2UiOiJhY2Nlc3MiLCJzY29wZSI6Im9wZW5pZCBlbWFpbCIsImF1dGhfdGltZSI6MTY2MDYzNzcxMiwiZXhwIjoxNjYwNzI0MTEyLCJpYXQiOjE2NjA2Mzc3MTIsImp0aSI6Ijk5MDJmMGFhLWM3YzEtNDQ2ZS1iYWYwLTA3MGQ0OWU1OGI4OCIsInVzZXJuYW1lIjoiOTk4OTcwM2ItZmJiMy00MWIxLTk5NTUtZTY1NjNmNGE1YzAzIn0.rTh_L1dr08xL3DqLMOXZumn4Nund2YSc0JizLZktuGlDaK9LtM42UAkd1iO9E270c8WG1W9wejM5Jq0kDu11HWCcZ78FB9Sd1CeQ5ruxsTbw6KdeSpBhwbJh4nhPG0PNq4zOglqkCiCDS-k1FjAY8Fd9Vce0RmLghnMjpj9Wlcu25R_wlDpG3lwLxpG1O_wsSLZDFDGExOwi4dQtlAdZM1T07KgCnJeSRaATc9HMDjGJWmqBSEr6jk2PYjonyMzpzE5h0GO3fQNCiqN-Nxst9o1kEnKBcZAe7yv9KTAjZwx9FSDRJZHpvrGTZP5AcjFiGAZWKneZsCKw0KwOnMm87g'
+          "Access-Token": 'eyJraWQiOiJ0eU9LVHFIV1J2UDlNOGRVNDVQaHhRZkZDTndqRDQ4R3ltbVg4RDhmVmpFPSIsImFsZyI6IlJTMjU2In0.eyJzdWIiOiI5OTg5NzAzYi1mYmIzLTQxYjEtOTk1NS1lNjU2M2Y0YTVjMDMiLCJpc3MiOiJodHRwczpcL1wvY29nbml0by1pZHAuYXAtc291dGgtMS5hbWF6b25hd3MuY29tXC9hcC1zb3V0aC0xX1ZiTUN4V1VxbyIsInZlcnNpb24iOjIsImNsaWVudF9pZCI6IjVkaDBpM3NjODh0N2Jwa2Q4b3FsMzMxYWVqIiwiZXZlbnRfaWQiOiIwZTNiMDJiNS05MTlhLTRlN2QtYmI1Ni1lY2VhMTU0NGI0OGIiLCJ0b2tlbl91c2UiOiJhY2Nlc3MiLCJzY29wZSI6Im9wZW5pZCBlbWFpbCIsImF1dGhfdGltZSI6MTY2MDcyNTMyMywiZXhwIjoxNjYwODExNzIzLCJpYXQiOjE2NjA3MjUzMjMsImp0aSI6ImNjNjE2YWU4LTk3MjEtNDYyYi1hZDUwLTAyZDc5MGY0NDRlZiIsInVzZXJuYW1lIjoiOTk4OTcwM2ItZmJiMy00MWIxLTk5NTUtZTY1NjNmNGE1YzAzIn0.O-CUgEeerS8eeWyxBCL7lYk9Rv3LCEkPo6PXkP3sYUJI2q_HurjBoMCuMifc23gYKxO4Yyef5eaMCKNUqfcMhKBlXlDpgjQKqRUAjYbQxgOnRJaA8FzpXSQsbnfxOkWAEEkyISvsaKg7mYztL-TsD_qS0QJ3J95fjIJJaN2Za5s1m7kIwxgVI6v4HK9TbrDCi50ADtKLS2Nclr8FvScoRJBOJY7q-bwQ7jHFtA90EbxR8FWFipdx-MB-QcdoBo1T4KsN-WSiMPWvy_9G1W43SKMBbQ2Iplv2vGMHvnS1xlmWx8ilXjtsT-uotv4TIeiWHSWzAiO13DujoCVZStRFzA'
          //  ,
           //  'access-control-allow-origin': '*',
           // "Access-Control-Allow-Headers": "X-Requested-With"
@@ -26,10 +26,9 @@ export default function AxGetSubjectAtt() {
         console.log(response.data);
 
         const rollno =response.data.data.Item;
-        setjsondata(rollno);
+        console.log(rollno);
+        setjsondata(rollno)
 
-        // const status = response.data.data.value;
-        // setjsonDepartment(status);
 
       } else {
         console.error("error");
@@ -43,19 +42,13 @@ export default function AxGetSubjectAtt() {
     fetchData();
   }, []);
   return (
-    <div>
-    <button onClick={fetchData}>Dept</button>{jsonData1}<br/>
-    {/* <button onClick={fetchData}>Subject</button>{jsonData} */}
-      {/* <table>
-  <tr>
-    <td>RollNo</td>
-    <td>Status</td>  
-  </tr>
-  <tr>
-    <td>{jsonData1}</td>
-    <td>{jsonData2}</td>
-  </tr>
-</table> */}
-      </div>
+
+<div>
+    
+      {Object.entries(jsonData).map(([key,value])=>{
+        return `${key} = ${value}`
+      })}
+      
+    </div> 
   )
 }
--- src/componentsPost/AxPostFetchTutoredStud.js
+++ src/componentsPost/AxPostFetchTutoredStud.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 
 <script src="https://js.stripe.com/v3/"></script>
@@ -8,29 +8,73 @@ const api = 'https://p2nzd3xj7i.execute-api.ap-south-1.amazonaws.com/dev/staff/f
 
 
 
-export default function AxPostFetchTutoredStud() {
+// export default function AxPostFetchTutoredStud() {
+//   const fetchData = async () => {
+//     try {
+//       const response = await axios.post(api, {collegeId: '19E234'} , {
+//         headers: {
+//           "Access-Token": 'eyJraWQiOiJ0eU9LVHFIV1J2UDlNOGRVNDVQaHhRZkZDTndqRDQ4R3ltbVg4RDhmVmpFPSIsImFsZyI6IlJTMjU2In0.eyJzdWIiOiI5OTg5NzAzYi1mYmIzLTQxYjEtOTk1NS1lNjU2M2Y0YTVjMDMiLCJpc3MiOiJodHRwczpcL1wvY29nbml0by1pZHAuYXAtc291dGgtMS5hbWF6b25hd3MuY29tXC9hcC1zb3V0aC0xX1ZiTUN4V1VxbyIsInZlcnNpb24iOjIsImNsaWVudF9pZCI6IjVkaDBpM3NjODh0N2Jwa2Q4b3FsMzMxYWVqIiwiZXZlbnRfaWQiOiIwZTNiMDJiNS05MTlhLTRlN2QtYmI1Ni1lY2VhMTU0NGI0OGIiLCJ0b2tlbl91c2UiOiJhY2Nlc3MiLCJzY29wZSI6Im9wZW5pZCBlbWFpbCIsImF1dGhfdGltZSI6MTY2MDcyNTMyMywiZXhwIjoxNjYwODExNzIzLCJpYXQiOjE2NjA3MjUzMjMsImp0aSI6ImNjNjE2YWU4LTk3MjEtNDYyYi1hZDUwLTAyZDc5MGY0NDRlZiIsInVzZXJuYW1lIjoiOTk4OTcwM2ItZmJiMy00MWIxLTk5NTUtZTY1NjNmNGE1YzAzIn0.O-CUgEeerS8eeWyxBCL7lYk9Rv3LCEkPo6PXkP3sYUJI2q_HurjBoMCuMifc23gYKxO4Yyef5eaMCKNUqfcMhKBlXlDpgjQKqRUAjYbQxgOnRJaA8FzpXSQsbnfxOkWAEEkyISvsaKg7mYztL-TsD_qS0QJ3J95fjIJJaN2Za5s1m7kIwxgVI6v4HK9TbrDCi50ADtKLS2Nclr8FvScoRJBOJY7q-bwQ7jHFtA90EbxR8FWFipdx-MB-QcdoBo1T4KsN-WSiMPWvy_9G1W43SKMBbQ2Iplv2vGMHvnS1xlmWx8ilXjtsT-uotv4TIeiWHSWzAiO13DujoCVZStRFzA'
+//          //  ,
+//           //  'access-control-allow-origin': '*',
+//           // "Access-Control-Allow-Headers": "X-Requested-With"
+//         }
+//       });
+const token = JSON.parse(sessionStorage.getItem('data'));
+//const tokens = token.data.subject;
+
+export default function AxGetfetchCourseAllocated() {
+  const [jsonData,setjsonData] =useState([]);
   const fetchData = async () => {
     try {
-      const response = await axios.post(api, {collegeId: '19E234'} , {
+      const response = await axios.get(api, {
         headers: {
-          "Access-Token": 'eyJraWQiOiJ0eU9LVHFIV1J2UDlNOGRVNDVQaHhRZkZDTndqRDQ4R3ltbVg4RDhmVmpFPSIsImFsZyI6IlJTMjU2In0.eyJzdWIiOiI5OTg5NzAzYi1mYmIzLTQxYjEtOTk1NS1lNjU2M2Y0YTVjMDMiLCJpc3MiOiJodHRwczpcL1wvY29nbml0by1pZHAuYXAtc291dGgtMS5hbWF6b25hd3MuY29tXC9hcC1zb3V0aC0xX1ZiTUN4V1VxbyIsInZlcnNpb24iOjIsImNsaWVudF9pZCI6IjVkaDBpM3NjODh0N2Jwa2Q4b3FsMzMxYWVqIiwiZXZlbnRfaWQiOiI5NGJmZGQ3My1hNTU1LTQyNTktYmUyNi0xZTUyMGQzNGY3NjQiLCJ0b2tlbl91c2UiOiJhY2Nlc3MiLCJzY29wZSI6Im9wZW5pZCBlbWFpbCIsImF1dGhfdGltZSI6MTY2MDEzOTYxOSwiZXhwIjoxNjYwMjI2MDE5LCJpYXQiOjE2NjAxMzk2MTksImp0aSI6IjZkM2Y5ZWFlLTcxYzYtNDg0YS05MTYwLTg1MGNiZjU1MDU3NSIsInVzZXJuYW1lIjoiOTk4OTcwM2ItZmJiMy00MWIxLTk5NTUtZTY1NjNmNGE1YzAzIn0.P63oUjiPNIMZy0RhlgoBhM_EoyMxww3NBZjzrNhDYIyflFBJtcJYDo1j35-nmlhpJ6sX_hQCffE1JbJp4Y7FRIBUGgFyLfPrls7BiP29E1GPr5mTkJuFryEh8W_aK5cjY2zQtaAmtt3a59_1QpeaY_ibZhwBSKMOLPT3uG7rTZLAMLThA69ZWtbCrXzdMFmgmoZNBevW9ZqcNksWE4p2UsarbhdFp5oLU4v-AQIQRqpIVxhfMuEIR67pEtwXj9uq5lZ-wRBrsMxdkvicNxRPnXZnZ6teMW0D3kuzWiB2p5fs6pquZ1I2jDlg0fo4Vfi53nT3_-SSaq7k3Jqr7Q-1PA'
-         //  ,
-          //  'access-control-allow-origin': '*',
-          // "Access-Control-Allow-Headers": "X-Requested-With"
+          "Access-Token": 'eyJraWQiOiJ0eU9LVHFIV1J2UDlNOGRVNDVQaHhRZkZDTndqRDQ4R3ltbVg4RDhmVmpFPSIsImFsZyI6IlJTMjU2In0.eyJzdWIiOiI5OTg5NzAzYi1mYmIzLTQxYjEtOTk1NS1lNjU2M2Y0YTVjMDMiLCJpc3MiOiJodHRwczpcL1wvY29nbml0by1pZHAuYXAtc291dGgtMS5hbWF6b25hd3MuY29tXC9hcC1zb3V0aC0xX1ZiTUN4V1VxbyIsInZlcnNpb24iOjIsImNsaWVudF9pZCI6IjVkaDBpM3NjODh0N2Jwa2Q4b3FsMzMxYWVqIiwiZXZlbnRfaWQiOiIwZTNiMDJiNS05MTlhLTRlN2QtYmI1Ni1lY2VhMTU0NGI0OGIiLCJ0b2tlbl91c2UiOiJhY2Nlc3MiLCJzY29wZSI6Im9wZW5pZCBlbWFpbCIsImF1dGhfdGltZSI6MTY2MDcyNTMyMywiZXhwIjoxNjYwODExNzIzLCJpYXQiOjE2NjA3MjUzMjMsImp0aSI6ImNjNjE2YWU4LTk3MjEtNDYyYi1hZDUwLTAyZDc5MGY0NDRlZiIsInVzZXJuYW1lIjoiOTk4OTcwM2ItZmJiMy00MWIxLTk5NTUtZTY1NjNmNGE1YzAzIn0.O-CUgEeerS8eeWyxBCL7lYk9Rv3LCEkPo6PXkP3sYUJI2q_HurjBoMCuMifc23gYKxO4Yyef5eaMCKNUqfcMhKBlXlDpgjQKqRUAjYbQxgOnRJaA8FzpXSQsbnfxOkWAEEkyISvsaKg7mYztL-TsD_qS0QJ3J95fjIJJaN2Za5s1m7kIwxgVI6v4HK9TbrDCi50ADtKLS2Nclr8FvScoRJBOJY7q-bwQ7jHFtA90EbxR8FWFipdx-MB-QcdoBo1T4KsN-WSiMPWvy_9G1W43SKMBbQ2Iplv2vGMHvnS1xlmWx8ilXjtsT-uotv4TIeiWHSWzAiO13DujoCVZStRFzA'
         }
       });
-      if (response.data) {
-        console.log(response.data);
-      } else {
-        console.error("error");
-      }
-    } catch (error) {
-      console.error(error);
-    }
-  }
+//       
 
-  useEffect(() => {
-    fetchData();
-  }, []);
-  return <div>Hi</div>
+
+
+if (response.data) {
+  console.log(response.data);
+  const ans =response.data.data.subject;
+  setjsonData(ans);
+
+ // const list = []
+  // products.forEach((response.data) => {
+  //   list.push(<li>{product}</li>)
+  // })
+  
+  
+} else {
+  console.error("error");
+}
+} catch (error) {
+console.error(error);
+}
+}
+
+useEffect(() => {
+fetchData();
+}, []);
+
+return (
+<div>
+{/* <button onClick={fetchData}>Default</button>{jsonData} */}
+<table>
+<tr>
+<td>Subject</td>
+
+</tr>
+<tr>
+{jsonData.map(jsonData => (  
+    <li>  
+      {jsonData}  
+    </li>  
+  ))}
+</tr>
+</table>
+</div>
+
+)
 }
